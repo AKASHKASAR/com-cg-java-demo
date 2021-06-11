1c7f6518155505695883c7b35ec9f4c7f02f1392
AKASHKASAR <akash.kasar20798@gmail.com>
2021-06-11 12:42:00 +0530

    update

--- src/com/cg/demo/multi/ThreadDemo.java
+++ src/com/cg/demo/multi/ThreadDemo.java
@@ -1,33 +1,33 @@
 package com.cg.demo.multi;
 
-public class ThreadDemo implements Runnable{
-	
+public class ThreadDemo implements Runnable {
+
 	void printLoop() {
 
-		for(int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			try {
 				Thread.sleep(250);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			System.out.println(i);
-			
+
 		}
 	}
+
 	public void run() {
 		this.printLoop();
 	}
-	
-		public static void main(String[] args) {
-		
-			
-			Thread obj = new Thread ((Runnable) new ThreadDemo());
-			Thread obj1 = new Thread ((Runnable) new ThreadDemo());
-			Thread obj2 = new Thread ((Runnable) new ThreadDemo());
-			
-			obj.start();
-			obj1.start();
-			obj2.start();
+
+	public static void main(String[] args) {
+
+		Thread obj = new Thread((Runnable) new ThreadDemo());
+		Thread obj1 = new Thread((Runnable) new ThreadDemo());
+		Thread obj2 = new Thread((Runnable) new ThreadDemo());
+
+		obj.start();
+		obj1.start();
+		obj2.start();
 //			obj.printLoop();
 //			obj1.printLoop();
 //			obj2.printLoop();
