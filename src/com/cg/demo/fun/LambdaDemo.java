c0b591770bb5626137c353cef56b084bdc300814
AKASHKASAR <akash.kasar20798@gmail.com>
2021-06-12 12:37:17 +0530

    update

---
+++ src/com/cg/demo/fun/LambdaDemo.java
@@ -0,0 +1,23 @@
+package com.cg.demo.fun;
+
+//1.By Implementing the Interface:
+//public class LambdaDemo implements MyInt {
+//	public static void main(String[] args) {
+//		LambdaDemo obj = new LambdaDemo();
+//		obj.absMethod();
+//	}
+//	@Override
+//	public void absMethod() {
+//		System.out.println("absMethod");
+//	}
+//}
+
+//2.By anonymous inner class 
+
+public class LambdaDemo{
+	public static void main(String[] args) {
+		
+		
+	}
+}
+
