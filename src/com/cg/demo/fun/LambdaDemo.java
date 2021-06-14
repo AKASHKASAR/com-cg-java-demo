ade456df196cc0208ceb4055fd368364e120efff
AKASHKASAR <akash.kasar20798@gmail.com>
2021-06-14 09:27:07 +0530

    update

--- src/com/cg/demo/fun/LambdaDemo.java
+++ src/com/cg/demo/fun/LambdaDemo.java
@@ -1,5 +1,7 @@
 package com.cg.demo.fun;
 
+import java.util.Scanner;
+
 //1.By Implementing the Interface:
 //public class LambdaDemo implements MyInt {
 //	public static void main(String[] args) {
@@ -17,7 +19,51 @@ package com.cg.demo.fun;
 public class LambdaDemo{
 	public static void main(String[] args) {
 		
-		
+		 Scanner in = new Scanner(System.in);
+	        System.out.println("1 Palindrome number");
+	        System.out.println("2 Prime number");
+	        System.out.println("3 Quit");
+	        System.out.print("Enter your choice: ");
+	        int choice = in.nextInt();
+	        System.out.print("Enter A number: ");
+	        int num = in.nextInt();
+
+	        switch (choice) {
+	            case 1:
+	            int copyNum = num;
+	            int revNum = 0;
+
+	            while(copyNum != 0) {
+	                int digit = copyNum % 10;
+	                copyNum /= 10;
+	                revNum = revNum * 10 + digit;
+	            }
+
+	            if (revNum == num) 
+	                System.out.print(num + " is palindrome");
+	            else
+	                System.out.print(num + " is not palindrome");
+	            break;
+
+	            case 2:
+	            	int c = 0;
+	                for (int i = 1; i <= num; i++) {
+	                    if (num % i == 0) {
+	                        c++;
+	                    }
+	                }
+	                if (c == 2) 
+	                    System.out.print(num + " is Prime");
+	                else
+	                    System.out.print(num + " is not Prime");
+	                break;
+
+	            default:
+	            System.out.println("quit");
+	            break;
+	        }
+	    
+	
 	}
 }
 
--- src/com/cg/demo/multi/ThreadDemo2.java
+++ src/com/cg/demo/multi/ThreadDemo2.java
@@ -1,5 +1,34 @@
 package com.cg.demo.multi;
 
-public class ThreadDemo2 {
+public class ThreadDemo2 extends Thread {
 
+	public void printLoop() {
+		for (int i = 1; i <= 10; i++) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+
+		}
+	}
+
+	@Override
+	public void run() {
+		this.printLoop();
+	}
+
+	public static void main(String[] args) {
+
+		ThreadDemo2 obj = new ThreadDemo2();
+		ThreadDemo2 obj2 = new ThreadDemo2();
+		ThreadDemo2 obj3 = new ThreadDemo2();
+//		obj.start();
+//		obj2.start();
+//		obj3.start();
+		obj.printLoop();
+		obj2.printLoop();
+		obj3.printLoop();
+	}
 }
